d8d1e19c68fc8062fa0ecc09270f8eeab823cc51
zack-horton <zhorton007@gmail.com>
2024-05-02 23:38:41 -0400

    updated and created post_scraping_all_fights_media

--- media_scrape.py
+++ media_scrape.py
@@ -1,6 +1,21 @@
 import pandas as pd
 import requests
+import re
+import unidecode
 from bs4 import BeautifulSoup
+
+def clean_names(name):
+    try:
+        name = unidecode.unidecode(name)
+        new = name.lower()
+        new = re.sub(r'[àáâãäå]', 'a', new)
+        new = re.sub(r'[èéêë]', 'e', new)
+        new = re.sub(r'[ìíîï]', 'i', new)
+        new = re.sub(r'[òóôõö]', 'o', new)
+        new = re.sub(r'[ùúûü]', 'u', new)
+        return new.title()
+    except:
+        name
 
 def get_media_scores(fighter_1, fighter_2, f1_w, fight_id, show=False):
     if fighter_1 == "Marco Polo Reyes":
@@ -47,7 +62,7 @@ def get_media_scores(fighter_1, fighter_2, f1_w, fight_id, show=False):
     
     url = f"http://mmadecisions.com/decision/{fight_id}/{fighter_1.replace(' ', '-') if f1_w == 1 else fighter_2.replace(' ', '-')}-vs-{fighter_1.replace(' ', '-') if f1_w == 0 else fighter_2.replace(' ', '-')}"
     
-    if print:
+    if show:
         print(url)
         
     payload = requests.get(url)
@@ -64,9 +79,10 @@ def get_media_scores(fighter_1, fighter_2, f1_w, fight_id, show=False):
     soup = soups[0]
     # print(soup)
     # print()
-    bits = soup.split(" ")
+    bits = soup.split("  ")
 
-    # print(bits)
+    if show:
+        print(bits)
     # print()
 
     fighter_1_count = 0
@@ -74,18 +90,29 @@ def get_media_scores(fighter_1, fighter_2, f1_w, fight_id, show=False):
     draw_count = 0
     scores_count = 0
     for i in range(0, len(bits)):
-        if bits[i] == "DRAW":
+        bit = bits[i]
+        if bit == "DRAW":
             draw_count += 1
-        elif (bits[i] in fighter_1) and (len(bits[i]) > 1):
-            fighter_1_count += 1
-        elif (bits[i] in fighter_2) and (len(bits[i]) > 1):
-            fighter_2_count += 1
+            
+        elif '-' not in bit:
+            if (any([True if clean_names(sub) in fighter_1 else False for sub in bit.split(" ")])) and (len(bit) > 1):
+                fighter_1_count += 1
+            elif (any([True if clean_names(sub) in fighter_2 else False for sub in bit.split(" ")])) and (len(bit) > 1):
+                fighter_2_count += 1
+                
+        elif '-' in bit:
+            sub_bits = bit.split('-')
+            if (any([True if clean_names(sub) in fighter_1 else False for sub_bit in sub_bits for sub in sub_bit.split(" ")])) and (len(bit) > 5):
+                fighter_1_count += 1
+            elif (any([True if clean_names(sub) in fighter_2 else False for sub_bit in sub_bits for sub in sub_bit.split(" ")])) and (len(bit) > 5):
+                fighter_2_count += 1
         
-        if ("-" in bits[i]) and not ("." in bits[i]):
-            scores_count += 1
+        if ("-" in bit) and not ("." in bit):
+            if (len(bit) == 5) and (bit[0].isnumeric()):
+                scores_count += 1
     return fighter_1_count, fighter_2_count, draw_count, scores_count
 
-if __name__ == "__main__":
+def alt_main():
     fighter_1 = "Alexander Volkanovski"
     fighter_2 = "Max Holloway"
     f1_w = 1
@@ -121,3 +148,34 @@ if __name__ == "__main__":
     fight_id = 9535
     scores = get_media_scores(fighter_1, fighter_2, f1_w, fight_id, show=True)
     print(f"\n{fighter_1} got {scores[0]} votes | {fighter_2} got {scores[1]} votes | {scores[2]} votes for draw | {scores[3]} total votes\n")
+    
+    fighter_1 = "Elias Theodorou"
+    fighter_2 = "Eryk Anders"
+    f1_w = 1
+    fight_id = 9630
+    scores = get_media_scores(fighter_1, fighter_2, f1_w, fight_id, show=True)
+    print(f"\n{fighter_1} got {scores[0]} votes | {fighter_2} got {scores[1]} votes | {scores[2]} votes for draw | {scores[3]} total votes\n")
+
+def main():
+    fights_df = pd.read_csv("post_scraping_all_fights.csv")
+    # fights_df = fights_df.iloc[:11, :]
+    fights_df['F1_media_votes'] = None
+    fights_df['F2_media_votes'] = None
+    fights_df['media_votes_draw'] = None
+    fights_df['media_votes_count'] = None
+    fights_df['media_votes_correct'] = False
+    
+    for idx, row in fights_df.iterrows():
+        output = get_media_scores(row['Fighter_1'], row['Fighter_2'], row['F1_W'], row['Fight_ID'])
+        fights_df.iloc[idx, list(fights_df.columns).index('F1_media_votes')] = output[0]
+        fights_df.iloc[idx, list(fights_df.columns).index('F2_media_votes')] = output[1]
+        fights_df.iloc[idx, list(fights_df.columns).index('media_votes_draw')] = output[2]
+        fights_df.iloc[idx, list(fights_df.columns).index('media_votes_count')] = output[3]
+        fights_df.iloc[idx, list(fights_df.columns).index('media_votes_correct')] = True if ((output[0] + output[1] + output[2]) == output[3]) else False
+        print(f'{idx} / {fights_df.shape[0]}')
+    
+    print(fights_df['media_votes_correct'].value_counts())
+    fights_df.to_csv("post_scraping_all_fights_media.csv", index=False)
+    
+if __name__ == "__main__":
+    alt_main()
